05b5ee70e043259d6b93069c9bcf8ef7b4102f27
Jiri Dusil <jdusil@trask.cz>
2022-08-10 08:22:07 +0200

    Chat - phase 1

---
+++ src/main/java/web/parujeme/application/views/MessageListek.java
@@ -0,0 +1,21 @@
+package web.parujeme.application.views;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
+
+/**
+ * @author jdusil
+ * @date 2022-08-09 5:22 PM
+ */
+
+public class MessageListek extends Div {
+    public MessageListek() {
+        addClassName("message-list");
+    }
+
+    @Override
+    public void add(Component... components) {
+        super.add(components);
+        components[components.length - 1].getElement().callFunction("scrollIntoView");
+    }
+}
